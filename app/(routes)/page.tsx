38505d11cc74c5dfcd89af34258c992558e43973
vSketch <davidazveb09242006@gmail.com>
2024-01-20 11:56:23 -0500

    read

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,24 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const products = await getProducts({ isFeatured: true });
+    const billboard = await getBillboard("3b6c7816-6023-4f2e-a370-8d55d827ed4f");
+    return (
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard} />
+                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                    <ProductList title="Featured Products" items={products} />
+                </div>
+            </div>
+        </Container>
+    );
+}
+
+export default HomePage;
