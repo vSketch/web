32f370f183bee5a19e0a3b681fdf373fbfb6b521
vSketch <davidazveb09242006@gmail.com>
2024-02-23 21:08:04 -0500

    Updated

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,16 +8,14 @@ export const revalidate = 0;
 
 const HomePage = async () => {
     const products = await getProducts({ isFeatured: true });
-    const billboard = await getBillboard("3b6c7816-6023-4f2e-a370-8d55d827ed4f");
+    const billboard = await getBillboard("f73b75c6-8ed5-4441-8cd7-d92941423cce");
     return (
-        <Container>
-            <div className="space-y-10 pb-10">
-                <Billboard data={billboard} />
-                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-                    <ProductList title="Featured Products" items={products} />
-                </div>
+        <div className="space-y-10 pb-10">
+            <Billboard data={billboard} />
+            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                <ProductList title="Featured Products" items={products} />
             </div>
-        </Container>
+        </div>
     );
 }
 
